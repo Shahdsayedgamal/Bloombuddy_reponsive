04a8c9040503690e42ec90077afa554a92fe9bd4
Shahdsayedgamal <Shahdsayed20022@gmail.com>
2024-04-01 00:59:43 +0200

    new update

--- lib/modules/land_scaping/land_scapping.dart
+++ lib/modules/land_scaping/land_scapping.dart
@@ -1,156 +1,175 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
-class LandscapeScreen extends StatelessWidget {
+class LandscapeScreen extends StatefulWidget {
+  @override
+  State<LandscapeScreen> createState() => _LandscapeScreenState();
+}
+
+class _LandscapeScreenState extends State<LandscapeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
       body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/login.jpg'), // Change to your image path
+              fit: BoxFit.cover,
+            ),
+          ),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: 10),
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(height: 80),
               Text(
-                'Your Partner in Landscaping Solutions',
+                'Landscape services',
                 style: TextStyle(
-                  fontSize: 18,
-                  color: Colors.grey[600],
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
                 ),
               ),
-              SizedBox(height: 30),
-              _buildContactInfo(
-                icon: Icons.phone,
-                label: 'Phone Number',
-                info: '012345678910',
+
+              SizedBox(height: 40),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 35.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(25.0),
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(25.0),
+                    child: BackdropFilter(
+                      filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+                      child: Container(
+                        padding: const EdgeInsets.all(15.0), // Adjusted padding
+                        decoration: BoxDecoration(
+                          color: Colors.white.withOpacity(0.3),
+                          borderRadius: BorderRadius.circular(25.0),
+                        ),
+                        child: Column(
+                          children: <Widget>[
+                            ListTile(
+                              leading: Icon(Icons.phone, color: Colors.white),
+                              title: Text(
+                                '+1 123-456-7890', // Sample phone number
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                            ListTile(
+                              leading: Icon(Icons.email, color: Colors.white),
+                              title: Text(
+                                'bloombuddy@gmail.com', // Sample email address
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 20),
+                            TextFormField(
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                              decoration: InputDecoration(
+                                labelText: 'Name',
+                                labelStyle: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                                enabledBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.white, width: 3),
+                                  borderRadius: BorderRadius.circular(25.0),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.white, width: 3),
+                                  borderRadius: BorderRadius.circular(25.0),
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 20),
+                            TextFormField(
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                              decoration: InputDecoration(
+                                labelText: 'Email',
+                                labelStyle: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                                enabledBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.white, width: 3),
+                                  borderRadius: BorderRadius.circular(25.0),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.white, width: 3),
+                                  borderRadius: BorderRadius.circular(25.0),
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 20),
+                            TextFormField(
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                              maxLines: 3,
+                              decoration: InputDecoration(
+                                labelText: 'Message',
+                                labelStyle: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                                enabledBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.white, width: 3),
+                                  borderRadius: BorderRadius.circular(25.0),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.white, width: 3),
+                                  borderRadius: BorderRadius.circular(25.0),
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 20),
+                            ElevatedButton(
+                              onPressed: () {
+                                // Send message action
+                              },
+                              style: ElevatedButton.styleFrom(
+                                primary: Colors.green[900],
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(25.0),
+                                ),
+                              ),
+                              child: Text(
+                                'Send Message',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
               ),
-              SizedBox(height: 20),
-              _buildContactInfo(
-                icon: Icons.email,
-                label: 'Email',
-                info: 'bloombuddy@gmail.com',
-              ),
-              SizedBox(height: 30),
-              _buildSectionTitle('Our Services'),
-              _buildServiceItem('Landscape Design'),
-              _buildServiceItem('Lawn Care and Maintenance'),
-              _buildServiceItem('Garden Installation'),
-              _buildServiceItem('Irrigation Systems'),
-              _buildServiceItem('Tree and Shrub Planting'),
-              SizedBox(height: 30),
-              _buildSectionTitle('Customer Reviews'),
-              _buildTestimonialItem(
-                name: 'John Doe',
-                review: 'Amazing work! Highly recommended.',
-              ),
-              _buildTestimonialItem(
-                name: 'Jane Smith',
-                review: 'Professional and efficient service.',
-              ),
-              SizedBox(height: 30),
-              _buildFooter(),
             ],
           ),
         ),
       ),
     );
   }
-
-  Widget _buildContactInfo({
-    required IconData icon,
-    required String label,
-    required String info,
-  }) {
-    return Row(
-      children: [
-        Icon(icon, color: Colors.green[900], size: 24),
-        SizedBox(width: 10),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              label,
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-                color: Colors.green[900],
-              ),
-            ),
-            Text(
-              info,
-              style: TextStyle(fontSize: 16, color: Colors.grey[600]),
-            ),
-          ],
-        ),
-      ],
-    );
-  }
-
-  Widget _buildSectionTitle(String title) {
-    return Text(
-      title,
-      style: TextStyle(
-        fontSize: 20,
-        fontWeight: FontWeight.bold,
-        color: Colors.green[900],
-      ),
-    );
-  }
-
-  Widget _buildServiceItem(String service) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 10, top: 5),
-      child: Row(
-        children: [
-          Icon(Icons.done, color: Colors.green[900], size: 20),
-          SizedBox(width: 10),
-          Text(
-            service,
-            style: TextStyle(fontSize: 16, color: Colors.grey[600]),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildTestimonialItem({required String name, required String review}) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        ListTile(
-          leading: CircleAvatar(
-            backgroundColor: Colors.green[900],
-            child: Icon(Icons.person, color: Colors.white),
-          ),
-          title: Text(
-            review,
-            style: TextStyle(fontSize: 16, color: Colors.grey[600]),
-          ),
-          subtitle: Text(
-            '- $name',
-            style: TextStyle(fontSize: 14, color: Colors.grey[500]),
-          ),
-        ),
-        SizedBox(height: 5),
-      ],
-    );
-  }
-
-  Widget _buildFooter() {
-    return Container(
-      color: Colors.green[900],
-      padding: EdgeInsets.all(20),
-      alignment: Alignment.center,
-      child: Text(
-        'Contact us!',
-        style: TextStyle(
-          color: Colors.white,
-          fontSize: 18,
-        ),
-        textAlign: TextAlign.center,
-      ),
-    );
-  }
 }
-
